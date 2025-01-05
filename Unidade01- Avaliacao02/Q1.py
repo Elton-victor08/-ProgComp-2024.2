18d8df8c903abdd2701e73a603eb5e0467b31808
Elton Victor <163954622+Elton-victor08@users.noreply.github.com>
2025-01-05 19:08:14 -0300

    Update Q1.py

--- Unidade01- Avaliacao02/Q1.py
+++ Unidade01- Avaliacao02/Q1.py
@@ -1,1 +1,23 @@
+#aluno: Elton Victor Da Costa - 20241014050024
+#aluna: Kaillany Felix Souza - 20242014050021
 
+
+contador = 0
+
+# Laço para percorrer os números de 10 até 987631
+for num in range(10, 987632): 
+    num_str = str(num)
+    decrescente = True
+
+    # Verificando se o número é decrescente
+    for i in range(len(num_str) - 1):
+        if num_str[i] < num_str[i + 1]: 
+            decrescente = False
+            break
+
+    # Se for decrescente, incrementa o contador
+    if decrescente:
+        contador += 1
+
+# Exibindo a quantidade de números decrescentes
+print(f"A quantidade de números decrescentes entre 10 e 987631 é: {contador}")
